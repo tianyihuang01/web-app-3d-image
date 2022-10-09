e08675771bf80b626d885f11c8fbf6fa219be5bb
Tianyi Huang <h.tianyi@outlook.com>
2022-10-09 21:57:49 +1100

    add videos and more styles

---
+++ script.js
@@ -0,0 +1,18 @@
+const ACTIVE = 'active';
+
+const active_video = document.querySelectorAll(
+	'.main-structure .content video'
+);
+
+const main_header = document.querySelector('.content .text-description h1');
+const second_header = document.querySelector('.content .text-description h2');
+const paragraph = document.querySelector('.content .text-description p');
+const text_link = document.querySelector('.content .text-description a');
+
+active_video[0].classList.remove(ACTIVE);
+active_video[0].classList.add(ACTIVE);
+
+main_header.innerHTML = CONTENT_INFO.island.h1;
+second_header.innerHTML = CONTENT_INFO.island.h2;
+paragraph.innerHTML = CONTENT_INFO.island.p;
+text_link.innerHTML = 'Explore';
---
+++ src/data/content.js
@@ -0,0 +1,27 @@
+const CONTENT_INFO = {
+	island: {
+		h1: 'Never Stop',
+		h2: 'Explore the World',
+		p: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas ut rerum nihil quia ab possimus explicabo officiis odit dicta delectus blanditiis, architecto, eos ducimus, animi aut? Adipisci aliquam ut beatae!',
+	},
+	spring: {
+		h1: 'Spring',
+		h2: 'Feel the new vitality',
+		p: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas ut rerum nihil quia ab possimus explicabo officiis odit dicta delectus blanditiis, architecto, eos ducimus, animi aut? Adipisci aliquam ut beatae!',
+	},
+	summer: {
+		h1: 'Summer',
+		h2: 'Enjoy passionate time',
+		p: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas ut rerum nihil quia ab possimus explicabo officiis odit dicta delectus blanditiis, architecto, eos ducimus, animi aut? Adipisci aliquam ut beatae!',
+	},
+	autumn: {
+		h1: 'Autumn',
+		h2: 'Experience without limits',
+		p: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas ut rerum nihil quia ab possimus explicabo officiis odit dicta delectus blanditiis, architecto, eos ducimus, animi aut? Adipisci aliquam ut beatae!',
+	},
+	winter: {
+		h1: 'Winter',
+		h2: 'Enjoy peace and freedom',
+		p: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas ut rerum nihil quia ab possimus explicabo officiis odit dicta delectus blanditiis, architecto, eos ducimus, animi aut? Adipisci aliquam ut beatae!',
+	},
+};
