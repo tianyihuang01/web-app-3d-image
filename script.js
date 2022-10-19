450dfabe2ce0927b2fb0e83734a917da8544cfb6
Tianyi Huang <h.tianyi@outlook.com>
2022-10-19 21:16:54 +1100

    add 3d effect to main structure

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const active_video = document.querySelectorAll(
 	'.main-structure .content video'
 );
 
+const menu_logo = document.querySelector('header .menu-logo');
+const main_structure = document.querySelector('.main-structure');
 const main_header = document.querySelector('.content .text-description h1');
 const second_header = document.querySelector('.content .text-description h2');
 const paragraph = document.querySelector('.content .text-description p');
@@ -16,3 +18,8 @@ main_header.innerHTML = CONTENT_INFO.island.h1;
 second_header.innerHTML = CONTENT_INFO.island.h2;
 paragraph.innerHTML = CONTENT_INFO.island.p;
 text_link.innerHTML = 'Explore';
+
+menu_logo.addEventListener('click', () => {
+	menu_logo.classList.toggle(ACTIVE);
+	main_structure.classList.toggle(ACTIVE);
+});
